e62e38234883ad7d490d1acba4838e7b6ad78100
geforcesong <georgeguo@outlook.com>
2019-03-06 15:35:34 -0800

    added snack bar and dialgue

--- lib/about.dart
+++ lib/about.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'SelectionScreen.dart';
 
 class AboutRoute extends StatelessWidget {
   @override
@@ -7,15 +8,46 @@ class AboutRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text("About Route"),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-            // Navigator.pushNamed(context, '/');
-          },
-          child: Text('Go back!'),
-        ),
+      // Create an inner BuildContext so that the onPressed methods
+      // can refer to the Scaffold with Scaffold.of().
+      body: Builder(
+        builder: (BuildContext context) {
+          return Center(
+            child: Column(
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    // Navigator.pushNamed(context, '/');
+                  },
+                  child: Text('Go back!'),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    _navigateAndDisplaySelection(context);
+                  },
+                  child: Text('Open Question!'),
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
+  }
+
+  _navigateAndDisplaySelection(BuildContext context) async {
+    // Navigator.push returns a Future that will complete after we call
+    // Navigator.pop on the Selection Screen!
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SelectionScreen()),
+    );
+
+    // After the Selection Screen returns a result, hide any previous snackbars
+    // and show the new result!
+    Scaffold.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(SnackBar(content: Text("$result")));
   }
 }
