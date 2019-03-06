38736adc98be200d2aa753254e1d4fe0df703c11
geforcesong <georgeguo@outlook.com>
2019-03-06 14:05:39 -0800

    added about page

---
+++ lib/about.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AboutRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("About Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
