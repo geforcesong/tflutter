57dca843ab79dd0c56589ddc1159aa9dda16f5cb
geforcesong <georgeguo@outlook.com>
2019-03-11 17:38:00 -0700

    added async func

--- lib/mpage/SearchPage.dart
+++ lib/mpage/SearchPage.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'models/MPageModel.dart';
 
 class SearchPage extends StatelessWidget {
   final String title;
   SearchPage(this.title);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
+    var modal = ScopedModel.of<MPageModel>(context);
+    modal.loadData();
     return Scaffold(
       appBar: AppBar(
         title: new Text(this.title),
@@ -15,7 +18,7 @@ class SearchPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('hello search page')
+            Text('hello search page1')
           ],
         ),
       )
--- lib/mpage/models/MPageModel.dart
+++ lib/mpage/models/MPageModel.dart
@@ -1,5 +1,34 @@
 import 'package:scoped_model/scoped_model.dart';
+import '../factories/PropertyFactory.dart';
+
+class Listing {
+  String address;
+  String id;
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "${this.address}, ${this.id}";
+  }
+}
 
 class MPageModel extends Model {
+  List<Listing> _listings = new List<Listing>();
 
+  loadData() async {
+    var propertyFactory = new PropertyFactory();
+    var resp = await propertyFactory.searchListings();
+    if (resp != null &&
+        resp["data"] != null &&
+        resp["data"]["listings"] != null) {
+      var listings = resp["data"]["listings"];
+      for (var i = 0; i < listings.length; i++) {
+        Listing lst = new Listing();
+        lst.address = listings[i]["address"];
+        lst.id = listings[i]["id"];
+        print(lst.toString());
+        _listings.add(lst);
+      }
+    }
+  }
 }
