d26ae2d3704b213d44d3053dbccf9e9cf8b784a6
geforcesong <georgeguo@outlook.com>
2019-03-12 17:28:01 -0700

    updated

--- lib/mpage/models/MPageModel.dart
+++ lib/mpage/models/MPageModel.dart
@@ -14,15 +14,23 @@ class Property {
 
 class MPageModel extends Model {
   List<Property> listings = new List<Property>();
+  int _pageIndex = 0;
+  int _totalPages = -1;
 
   int get listingCount => listings.length;
 
   loadData() async {
+    _pageIndex++;
+    if (_totalPages > 0 && _pageIndex >= _totalPages) {
+      return;
+    }
     var propertyFactory = new PropertyFactory();
-    var resp = await propertyFactory.searchListings();
+    var resp = await propertyFactory.searchListings(_pageIndex);
     if (resp != null &&
         resp["data"] != null &&
         resp["data"]["listings"] != null) {
+      _totalPages = (resp["data"]["totalCount"] ~/ 50) +
+          (resp["data"]["totalCount"] % 50 == 0 ? 0 : 1);
       var respListings = resp["data"]["listings"];
       for (var i = 0; i < respListings.length; i++) {
         Property property = new Property();
