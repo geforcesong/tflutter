9dfc78adbe34392b98167ab8ce5f23b8cfbb23a4
geforcesong <georgeguo@outlook.com>
2019-03-15 15:39:31 -0700

    build slide pages

--- lib/bottombar/screens/page1.dart
+++ lib/bottombar/screens/page1.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 
-class Page1 extends StatelessWidget{
+class Page1 extends StatelessWidget {
+  Widget _buildPage(int index, Color color) {
+    return Container(
+        alignment: AlignmentDirectional.center,
+        color: color,
+        child: Text('$index',
+            style: TextStyle(fontSize: 132, color: Colors.white)));
+  }
+
+  Widget _buildPageView() {
+    return PageView(
+      children: [
+        _buildPage(1, Colors.green),
+        _buildPage(2, Colors.blue),
+        _buildPage(3, Colors.indigo),
+        _buildPage(4, Colors.red),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Page1');
+    return _buildPageView();
   }
 }
