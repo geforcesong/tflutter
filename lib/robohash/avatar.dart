94428a3940199535396a48e130945ffed9e85b3d
geforcesong <georgeguo@outlook.com>
2019-03-25 15:40:25 -0700

    fix avartar

--- lib/robohash/avatar.dart
+++ lib/robohash/avatar.dart
@@ -12,30 +12,22 @@ class Avartar extends StatefulWidget {
 }
 
 class _Avartar extends State<Avartar> {
-  Uint8List imageData;
-  String imageUrl;
+  Future<Uint8List> imageData;
 
   Future<Uint8List> fetchAvatar() async {
-    // print('widgetUrl: ${widget.url}');
-
-    if(imageUrl ==widget.url){
-      return imageData;
-    }
-
-    var start = DateTime.now();
-    // await Future.delayed(const Duration(seconds: 4));
     http.Response response = await http.get(widget.url);
-    var end = DateTime.now();
-    var difference = end.difference(start);
-    imageData =response.bodyBytes;
-    imageUrl =widget.url;
-    print('cost: ${difference.inMilliseconds}, url:${imageUrl}');
     return response.bodyBytes;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    imageData = fetchAvatar();
   }
 
   Widget loadingWidget() {
     return new FutureBuilder<Uint8List>(
-      future: fetchAvatar(),
+      future: imageData,
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.waiting:
