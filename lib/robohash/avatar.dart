ba2be634e3f0c0dd454be22e64a81ae746d94dae
geforcesong <georgeguo@outlook.com>
2019-03-25 08:50:37 -0700

    updated filter

--- lib/robohash/avatar.dart
+++ lib/robohash/avatar.dart
@@ -2,37 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:typed_data';
 
-class Avartar extends StatefulWidget{
-
+class Avartar extends StatefulWidget {
   Avartar({this.url, this.size});
   final String url;
   final double size;
 
   @override
-  State<StatefulWidget> createState() =>_Avartar();
+  State<StatefulWidget> createState() => _Avartar();
 }
 
 class _Avartar extends State<Avartar> {
+  Uint8List imageData;
+  String imageUrl;
 
-  Future<Uint8List> fetchAvatar() async{
-    http.Response response =await http.get(widget.url);
+  Future<Uint8List> fetchAvatar() async {
+    // print('widgetUrl: ${widget.url}');
+
+    if(imageUrl ==widget.url){
+      return imageData;
+    }
+
+    var start = DateTime.now();
+    // await Future.delayed(const Duration(seconds: 4));
+    http.Response response = await http.get(widget.url);
+    var end = DateTime.now();
+    var difference = end.difference(start);
+    imageData =response.bodyBytes;
+    imageUrl =widget.url;
+    print('cost: ${difference.inMilliseconds}, url:${imageUrl}');
     return response.bodyBytes;
   }
 
-  Widget loadingWidget(){
+  Widget loadingWidget() {
     return new FutureBuilder<Uint8List>(
       future: fetchAvatar(),
-      builder: (context, snapshot){
-        if(snapshot.hasData){
-          return Image.memory(snapshot.data);
-        } else if(snapshot.hasError){
-          print('${snapshot.error}');
-          return new Center(child: Text('Error'));
-        } else{
-          return Container(
-            padding: EdgeInsets.all(widget.size-200),
-            child: CircularProgressIndicator(),
-          );
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+          case ConnectionState.active:
+          case ConnectionState.none:
+            return Container(
+              padding: EdgeInsets.all(widget.size - 200),
+              child: CircularProgressIndicator(),
+            );
+            break;
+          default:
+            if (imageData!=null) {
+              return Image.memory(snapshot.data);
+            } else if (snapshot.hasError) {
+              print('${snapshot.error}');
+              return new Center(child: Text('Error'));
+            }
         }
       },
     );
@@ -41,26 +61,17 @@ class _Avartar extends State<Avartar> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: widget.size,
-      height: widget.size,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(
-          color: Colors.blue,
-          width: 3
-        ),
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end:Alignment.bottomCenter,
-          colors: [
-            Colors.transparent,
-            Colors.black12
-          ]
-        )
-      ),
-      child: ClipOval(
-        child: loadingWidget(),
-      )
-    );
+        width: widget.size,
+        height: widget.size,
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: Colors.blue, width: 3),
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.transparent, Colors.black12])),
+        child: ClipOval(
+          child: loadingWidget(),
+        ));
   }
 }
